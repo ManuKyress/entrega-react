321eeb0521bd8c015afb7a1fb822b348342c52fd
ManuKyress <lv.kyress@hotmail.com>
2025-12-04 08:50:59 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import styles from './Footer.module.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   
@@ -26,6 +27,7 @@ const Footer = () => {
 
 
 export default Footer;
+
 
 
 
